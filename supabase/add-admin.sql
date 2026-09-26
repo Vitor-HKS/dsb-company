bcefd64ce167644e80dd42dd0941011909e64012
Vitor-HKS <vitor.he2016@gmail.com>
2026-09-26 03:14:58 -0300

    Modify add-admin.sql to include new admin users
    Updated SQL script to insert admin users with specific emails.

--- supabase/add-admin.sql
+++ supabase/add-admin.sql
@@ -1,8 +1,8 @@
 -- Após criar o usuário no Dashboard > Authentication > Users, informe aqui o e-mail dele.
 insert into public.admin_users (id)
-select id from auth.users where email = 'vitor.he2016@gmail.com'
+select id from auth.users where email = ''
 on conflict (id) do nothing;
 insert into public.admin_users (id)
-select id from auth.users where email = 'leo.lemos@outlook.com'
+select id from auth.users where email = ''
 on conflict (id) do nothing;
 
