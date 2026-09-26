4680c92f0bb017d040c0946db7d14aaf312d406e
Vitor-HKS <vitor.he2016@gmail.com>
2026-09-26 03:27:18 -0300

    Update cms-config.js

--- js/cms-config.js
+++ js/cms-config.js
@@ -1,5 +1,5 @@
 // Cole apenas a URL e a chave PUBLICÁVEL do projeto. Nunca use secret/service_role aqui.
 window.DSB_CMS = {
-  url: '',
-  publishableKey: ''
-};
+  url: 'https://mjxydxbnfpreidizkepk.supabase.co',
+  publishableKey: 'sb_publishable__5qntz4Z7sTlcHvNf3UfOQ_-aCjEABb'
+ };
